b7c52b36226c2ff31c6d8e870862aee5593c4ebc
abutalha08 <taufique752@gmail.com>
2026-04-15 03:21:35 +0600

    Created Your Friends section and make it responsive

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../components/Banner/Banner';
+import AllFriends from '../../components/AllFriends/AllFriends';
 
 const Homepage = () => {
     return (
         <div>
             <Banner></Banner>
+            <AllFriends></AllFriends>
         </div>
     );
 };
