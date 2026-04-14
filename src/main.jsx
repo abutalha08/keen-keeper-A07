489abeece7b6823e5c2e3299c7445c8f4afae698
abutalha08 <taufique752@gmail.com>
2026-04-14 20:10:28 +0600

    Folder structure created

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router';
+import MainLayout from './layout/MainLayout';
+import Homepage from './pages/HomePage/Homepage';
+import Timeline from './pages/Timeline/Timeline';
+import Stats from './pages/Stats/Stats';
 
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        index: true,
+        element: <Homepage></Homepage>
+      },
+      {
+        path: "/timeline",
+        element: <Timeline></Timeline>
+
+      },
+      {
+        path: "/stats",
+        element: <Stats></Stats>
+      }
+    ]
   },
 ]);
 
